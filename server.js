d3b8501662423c4ac68ed8932214330e27945dbc
ZacWJ1 <142571679+ZacWJ1@users.noreply.github.com>
2025-02-10 21:47:30 -0800

    updated cors eight

--- server.js
+++ server.js
@@ -16,12 +16,9 @@ require('./config/passport')(passport)
 
 const flash = require('express-flash')
 const logger = require('morgan')
-const corsOptions = {
-  origin: 'https://groceries2-frontend.onrender.com',
-  credentials: true // This ensures that credentials (cookies) are allowed
-};
+
 const app = express();
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', req.headers.origin);
