46291d3d0c5d596aa528b8d82d61e989456e7353
ZacWJ1 <142571679+ZacWJ1@users.noreply.github.com>
2025-02-10 21:36:52 -0800

    updated cors seven

--- server.js
+++ server.js
@@ -90,7 +90,6 @@ app.post("/login", async (req, res) => {
       if (user) {
           const passwordMatch = await bcrypt.compare(password, user.password);
           if (passwordMatch) {
-            res.header('Access-Control-Allow-Origin',"https://groceries2-frontend.onrender.com")
               req.session.user = { id: user._id, name: user.name, email: user.email };
               // console.log(email);
                req.session.save((err) => {
@@ -133,7 +132,7 @@ app.get('/user', (req, res) => {
     console.log('Session data:', req.session);
   if (req.session.user) {
     console.log("Authenticated user:", req.session.user);
-      res.header('Access-Control-Allow-Origin',"https://groceries2-frontend.onrender.com")
+    
       res.json({ user: req.session.user });
   } else {
     console.log("User not authenticated");
