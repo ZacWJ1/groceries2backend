7bb41106d040dfa6f632d6d39fffd41d5e1120f3
ZacWJ1 <142571679+ZacWJ1@users.noreply.github.com>
2025-02-10 23:05:40 -0800

    updated cors elv

--- server.js
+++ server.js
@@ -18,10 +18,14 @@ const flash = require('express-flash')
 const logger = require('morgan')
 
 const app = express();
-app.use(cors())
-response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT"); 
-response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-response.setHeader("Access-Control-Allow-Origin", "https://groceries2-frontend.onrender.com");
+const corsOptions = {
+  origin: 'https://groceries2-frontend.onrender.com/login', // Specify your frontend domain
+  credentials: true // This ensures that credentials (cookies) are allowed
+};
+app.use(cors(corsOptions))
+
+
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', req.headers.origin);
